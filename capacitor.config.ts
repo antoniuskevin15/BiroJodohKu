dd67e71aeed4086b926b5dbc349ba3c859c381b6
antoniuskevin15 <antoniuskevin15@gmail.com>
2022-10-03 12:38:20 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AntoniusKevin_00000045444_IF670_A_UTS',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
